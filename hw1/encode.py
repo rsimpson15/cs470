76664a194680061b8858a6dd1a96a0e34c7d6980
Rochelle <rsimpson15@wou.edu>
2018-09-27 08:47:21 -0700

    Added basic solution to encode - does not handle ext needs.

--- hw1/encode.py
+++ hw1/encode.py
@@ -1,2 +1,16 @@
+
+#Solution partially credited to hltbra: https://gist.github.com/hltbra/4117933
 def encode(input_string):
-     return ""
+     if not input_string: 
+          return ""
+     else:
+          last_char = input_string[0]
+          max_index = len(input_string)
+          i = 1
+          while i < max_index and last_char == input_string[i]:
+               i += 1
+          return last_char + str(i) + encode(text[i:])
+
+
+
+   
