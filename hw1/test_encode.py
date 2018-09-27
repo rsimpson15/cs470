8717840fe7805110c565e5b478e69901eed14a28
Rochelle <rsimpson15@wou.edu>
2018-09-27 08:35:23 -0700

    Added a test for expected string.

--- hw1/test_encode.py
+++ hw1/test_encode.py
@@ -1,6 +1,8 @@
-.txt
-
 from encode import encode
 
 def test_empty_string():
      assert encode("") == ""
+
+def test_expected_string():
+     input = "aaabbbcccddd"
+     assert encode(input) == "a3b3c3d3"
